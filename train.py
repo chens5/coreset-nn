801715c955c605dce1eb8ecbab709e6e56135a66
orenciolli <88753238+orenciolli@users.noreply.github.com>
2024-07-15 15:19:40 -0700

    softmax across dim=0

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from torch.optim import Adam
 from torch.nn import CrossEntropyLoss, NLLLoss
 from torch.utils.data import DataLoader
 from geotorch.sphere import uniform_init_sphere_ as unif_sphere
+from scipy.spatial.distance import directed_hausdorff
+
 
 import argparse
 import os
@@ -81,11 +83,18 @@ def format_log_dir(output_dir,
         os.makedirs(log_dir)
     return log_dir
 
+def centroid_distance(a, b):
+    centroid_a = torch.mean(a, axis=0)
+    distances = torch.norm(b - centroid_a, dim=1)
+    
+    return torch.mean(distances)
+
+
 # TODO: Given P and an approximate Q and n directions, we should compute
 # \sum_{i = 1}^n abs(max_p <u_i, p> - max_q <u_i, q>) + \sum_i^n abs(min_p <u_i, q> - min_q <u_i, q>)
 def direction_loss(p, q, directions, n=100, in_dim=3, device='cuda:0'):
     # directions = unif_sphere(torch.zeros(n,in_dim))
-    directions = directions.t().to(device)
+    directions = directions.t().double().to(device)
     p, q = p.to(device), q.to(device)
 
     
@@ -108,18 +117,18 @@ def direction_loss(p, q, directions, n=100, in_dim=3, device='cuda:0'):
     losses = []
     
     for i in range(len(p_slices)):
-        projections_q = (q_slices[i] @ directions).unsqueeze(dim=0)
-        projections_p = (p_slices[i] @ directions).unsqueeze(dim=0)
+        projections_q = (q_slices[i].double() @ directions).unsqueeze(dim=0)
+        projections_p = (p_slices[i].double() @ directions).unsqueeze(dim=0)
 
         max_q = torch.max(projections_q, dim=1)[0]
         min_q = torch.min(projections_q, dim=1)[0]
         max_p = torch.max(projections_p, dim=1)[0]
         min_p = torch.min(projections_p, dim=1)[0]
-   
+    
         losses.append(torch.sum(abs(max_q - max_p) + abs(min_q - min_p)))
     
 
-    return torch.mean(torch.stack(losses))
+    return torch.mean(torch.stack(losses)) #- centroid_distance(q.data, p.data)
 
 # TODO: Cross entropy loss for n directions
 def cross_entropy_loss():
@@ -131,7 +140,7 @@ def compute_test_error(model, test_dataloader, test_gt, test_sz, device='cuda:0'
     directions = unif_sphere(torch.zeros(50, 2)) #hardcoding size of point set and in_dim
     for batch in test_dataloader:
         batch = batch.to(device)
-        out = F.softmax(model(batch), dim=1)
+        out = F.softmax(model(batch), dim=0) #old model
         hulls = Batch.from_list(get_approx_chull(out, batch), order = 1)
         gt_p_batch = Batch.from_list(test_gt[count], order = 1)
         # ground_truth = gt_p_batch[i].to(device)
@@ -140,6 +149,42 @@ def compute_test_error(model, test_dataloader, test_gt, test_sz, device='cuda:0'
         count += 1
     loss = loss/test_sz
     return loss
+
+
+def gen_model_output(model, train_dataloader, test_dataloader, log_dir, epoch, device='cuda:0'):
+    model.to(device)
+
+    train_hulls = []
+    test_hulls = []
+
+    for batch in train_dataloader:
+        batch = batch.to(device)
+        out = F.softmax(model(batch), dim=0)
+        # out = model(batch) #new model
+
+        train_hulls += [tensor.cpu().detach().numpy() for tensor in get_approx_chull(out, batch)]
+            
+    model_output_dir = os.path.join(log_dir, 'output')
+    if not os.path.exists(model_output_dir):
+        os.makedirs(model_output_dir)
+
+
+    train_hull_file = f'chull_train_e{epoch}'
+    np.save(os.path.join(model_output_dir, train_hull_file), np.array(train_hulls))
+    print(f'output saved to {train_hull_file}')
+
+    for batch in test_dataloader:
+        batch = batch.to(device)
+        out = F.softmax(model(batch), dim=0)
+        # out = model(batch)
+
+        test_hulls += [tensor.cpu().detach().numpy() for tensor in get_approx_chull(out, batch)]
+            
+       
+    test_hull_file = f'chull_test_e{epoch}'
+    np.save(os.path.join(model_output_dir, test_hull_file), np.array(test_hulls))
+    print(f'output saved to {test_hull_file}')
+
 
 def train(modeltype, config, train_dataloader, train_gt, test_dataloader, test_gt, device, log_dir,
           epochs=100, lr=0.001, activation='LeakyReLU', test_sz = 300, save_freq=20):
@@ -168,12 +213,22 @@ def train(modeltype, config, train_dataloader, train_gt, test_dataloader, test_g
     for epoch in trange(epochs):
         count = 0
         total_loss = 0.0
-        directions = unif_sphere(torch.zeros(50, 2)) #hardcoding size of point set and in_dim
+        
+        #generating directions -- hardcoding for 2d
+        angles = np.linspace(0, 2 * np.pi, 100, endpoint=False)
+        x = np.cos(angles)
+        y = np.sin(angles)
+
+        x_tensor = torch.tensor(x)
+        y_tensor = torch.tensor(y)
+        directions = torch.stack([x_tensor, y_tensor], dim=1)
+
+
         for batch in train_dataloader:
 
             optimizer.zero_grad()
             batch = batch.to(device)
-            out = F.softmax(model(batch), dim=1)
+            out = F.softmax(model(batch), dim=0) #changed to dim=0
             hulls = Batch.from_list(get_approx_chull(out, batch), order = 1)
             gt_p_batch = Batch.from_list(train_gt[count], order = 1)
             
@@ -184,23 +239,26 @@ def train(modeltype, config, train_dataloader, train_gt, test_dataloader, test_g
             optimizer.step()
             count +=1
 
-        #saving output
-        if epoch == epochs - 1:
-            hulls = [tensor.cpu().detach().numpy() for tensor in get_approx_chull(out, batch)]
-            
-            model_output_dir = os.path.join(log_dir, 'output')
-            if not os.path.exists(model_output_dir):
-                os.makedirs(model_output_dir)
-            np.save(os.path.join(model_output_dir, 'chull_test'), np.array(hulls))
-            print('output saved')
-            hulls = [torch.tensor(arr) for arr in hulls]
-            hulls = Batch.from_list(hulls, order = 1) #casting back to batch
 
         # compute test error
         writer.add_scalar('train/mse_loss', total_loss/count, epoch)
         if epoch % save_freq == 0:
             path = os.path.join(record_dir, f'model_{epoch}.pt')
             torch.save(model.state_dict(), path)
+
+            #saving output
+            # gen_model_output(model, train_dataloader, test_dataloader, log_dir, epoch, device)
+
+            # hulls = [tensor.cpu().detach().numpy() for tensor in get_approx_chull(out, batch)]
+            # print(np.array(hulls))
+            
+            # model_output_dir = os.path.join(log_dir, 'output')
+            # if not os.path.exists(model_output_dir):
+            #     os.makedirs(model_output_dir)
+            # np.save(os.path.join(model_output_dir, f'chull_test_e{epoch}'), np.array(hulls))
+            # print('output saved')
+            # hulls = [torch.tensor(arr) for arr in hulls]
+            # hulls = Batch.from_list(hulls, order = 1) #casting back to batch
     
     test_err = compute_test_error(model, test_dataloader, test_gt, 300, device = device)
     print("test error:", test_err)
@@ -251,7 +309,7 @@ def main():
 
         output = train(args.layer_type, config, train_batches,train_gt, 
                        test_batches, test_gt, args.device, log_dir, 
-                       epochs=args.epochs, save_freq = 5) #added save_freq
+                       epochs=args.epochs, save_freq = 20) #added save_freq
         
         loss_data.append({'modelname':modelname, 'loss':output.item()})
 
